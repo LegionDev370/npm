aa93c49b67f6b1de086fc5a849044c82f0fb8788
LegionDev370 <llegion630@gmail.com>
2024-11-11 19:20:12 +0500

    pushed

---
+++ app.js
@@ -0,0 +1,24 @@
+// globalThis.window = "window";
+// globalThis.a = 23;
+
+// globalThis.fetch = function () {
+//   return {
+//     then: function (callback) {
+//       callback("Hello response");
+//     },
+//   };
+// };
+
+// fetch("https://daryo.uz").then((response) => console.log(response));
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+// console.log(process.cwd());
+
+// console.log("Hello nodejs");
+// console.log("World");
+
+// for (let i = 0; i < 12; i++) {
+//   console.log(i);
+// }
